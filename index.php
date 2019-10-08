9deed54fa1532da31d172d0d93b506b10b4beab1
Schiz667 <vhershlag@gmail.com>
2019-10-08 03:10:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$title = "Sygi";
+$body  = " SyGi is a Synthetic Girl(s)";
+?>
+<html>
+<head>
+    <title><?php print $title;?></title>
+</head>
+<body>
+<?php print $body; ?>
+</body>
+</html>
